f4c4429b7f823235250a53b6f547b43c36d8ef2a
willowj <765755345@qq.com>
2018-01-10 15:26:59 +0800

    Initial commit

---
+++ example/custom.js
@@ -0,0 +1,142 @@
+// #
+// # author: willowj
+// # date: 2018-01-10 13:35:18
+// #
+
+/*function search_query(query) {
+    times= 0
+     //alert("JSON Data: " + json.pages[0]["tags"] );
+}*/
+
+
+function search_query(query) {
+    var a = 10;
+    $.getJSON("/tipuesearch_content.json", function(json){
+
+        var search_resurltold = document.getElementsByClassName("search_resurlt")
+        if (search_resurltold.length>0){
+            search_resurltold[0].parentNode.removeChild(search_resurltold[0]);
+
+        };
+
+        var search_resurlt = search_text_in_json(json, query.trim())
+
+        var searchnavbar = document.getElementById("navbar_search_re");
+       // searchnavbar = searchnavbar[searchnavbar.length-1];
+        searchnavbar.appendChild(search_resurlt);
+
+    });
+}
+
+function search_text_in_json(json_, query) {
+    // body
+
+    var pages = json_.pages;
+    var search_resurlt=document.createElement("div");
+    search_resurlt.setAttribute("class" ,"search_resurlt");
+    search_resurlt.setAttribute("id" ,"search_resurlt");
+
+    var res = 0;
+    for (var i = pages.length - 1; i >= 0; i--) {
+        page_i = pages[i];
+        var index = 0;
+        var re_itme = "title";
+        index = page_i["title"].indexOf(query);
+        if (index < 0){
+            var re_itme = "url";
+            index = page_i["url"].indexOf(query);
+        };
+        if (index < 0){
+            var re_itme = "text";
+            index = page_i["text"].indexOf(query);
+        };
+
+        if (index >= 0){
+           // alert(index);
+            res = res + 1;
+            if (res==1 & document.getElementById("close_bun") == undefined){
+            faclose = document.createElement("button");
+            faclose.setAttribute("class", "menu-item-icon fa fa-close fa-fw");
+            faclose.setAttribute("id", "close_bun");
+            faclose.setAttribute("onclick", "close_search_result()");
+            navbar_search_re = document.getElementById("navbar_search_re");
+            navbar_search_re.appendChild(faclose);
+            // close button > close_search_result
+            }
+            // results dom
+            var article=document.createElement("article");
+            article.setAttribute("class", "search_resurlt_item");
+
+            var a = document.createElement("a");
+            a.setAttribute("href", page_i["url"]);
+            a.innerHTML = page_i["title"];
+
+
+            var br = document.createElement("br");
+            article.appendChild(a);
+            article.appendChild(br);
+
+
+            var elei = document.createElement("i");
+            elei.setAttribute("class", "fa fa-folder-open padding-small");
+            article.appendChild(elei);
+
+            var a2 = document.createElement("a");
+            a2.setAttribute("href",
+                "/category/{0}.html".format(page_i["tags"])
+                );
+            a2.innerHTML = page_i["tags"];
+            article.appendChild(a2)
+
+            var br = document.createElement("br");
+            article.appendChild(br)
+
+            // reasult  and search_key strong
+            var myText1 = document.createTextNode(page_i[re_itme].substring(index-20, index))
+            var b = document.createElement("b")
+            b.setAttribute("class", "search_key");
+            b.innerHTML = page_i[re_itme].substring(index, index+query.length)
+
+            var myText2 = document.createTextNode( page_i[re_itme].substring(index+query.length, index+query.length+30))
+
+            var pragraph = document.createElement("a")
+            pragraph.setAttribute("class", "summary");
+
+            pragraph.appendChild(myText1)
+            pragraph.appendChild(b)
+            pragraph.appendChild(myText2)
+            // reasult  and search_key strong
+
+            article.appendChild(pragraph)
+            search_resurlt.appendChild(article);
+        };
+    };
+    if (res==0) {
+        search_resurlt.innerHTML= "no results";
+    }
+    return search_resurlt;
+}
+
+
+function close_search_result() {
+    search_resurlt = document.getElementById("search_resurlt")
+    search_resurlt.setAttribute("style","display:none")
+
+    close_bun = document.getElementById('close_bun')
+    close_bun.parentNode.removeChild(close_bun)
+}
+
+String.prototype.format = function(arg) {
+    var result = this; //.split("").join("");
+    reg = /\{\d*\}/g ;
+    res = reg.exec(result);
+    if(res) {
+         for(var i = 0; i < arguments.length; i++) {
+            var n = i;
+            result = result.replace(eval('/\\{' + i + '\\}/g'),
+                String(arguments[i])
+                );
+            };
+    };
+return result;
+};
---
+++ readme_zh.md
@@ -0,0 +1,71 @@
+
+
+###pelican静态博客  搜索栏  Js工具
+
+基于tipuesearch 插件生成文章json数据；在pelican 建立的静态博客添加搜索工具对全部md文章搜索；效果如下：
+
+本质还是  json数据 jQuery查询 ，也可以改写 用在其他web前端
+
+![](http://images2017.cnblogs.com/blog/1083549/201801/1083549-20180110132641738-1281463823.gif)
+
+try：
+
+- 文件夹 打开命令行 
+- enter `python -m SimpleHTTPServer 8081`
+- http://localhost:8081/  /浏览器打开
+
+
+如果是其他插件生成的 md 内容结构，需要修改 js脚本
+
+
+[github >tipuesearch](https://github.com/getpelican/pelican-plugins/tree/master/tipue_search) 生成的文件名 为 tipuesearch_content.json，文档结构如下
+
+~~~json
+{"pages": 
+    [  
+        { "text": "Lorem ipsum dolor sit amet, consectetur ",
+            "tags": "Example Category",
+            "url" : "http://oncoot.com/plugin-example.html",
+            "title": "Everything you warem Ipsum"
+        },
+        { "text": "Sed ",
+            "tags": "Example Category",
+            "url" : "http://oncoot.com/plugin-example-2.html",
+            "title": "Review of the book Lorem Ipsum"
+        }
+    ]
+}
+~~~
+
+
+
+###pelican usage:
+
+- 在 pelicanconf.py 配置文件 添加  [tipuesearch](https://github.com/getpelican/pelican-plugins/tree/master/tipue_search)  插件
+
+  ​
+
+
+- 主题中 选择模板 添加  搜索表单
+
+  ​
+
+- 添加 脚本到自己的custom.js，或者直接放置到output folder,  对应修改 脚本来源路径   `<script src="/custom.js"></script>`
+
+  ​
+
+- 可以自己改写css样式 或者用别。  pelicanconf.py中指定 `CUSTOM_CSS = "/custom.css"` ，或者在html 模板中指定 css路径   `<link href="/custom.css" rel="stylesheet" type="text/css" />`。
+
+  ​
+
+- 渲染模板"make html"；    "make publish"会清空output 文件夹
+
+  ​
+
+- serve
+
+  ​
+
+###enjoy it
+
+recommend  "pelican-toc" plugin for md contents
